856d4d1aaba8a0b196b0ab8a42c1bb1050075f38
marath007 <36909715+marath007@users.noreply.github.com>
2020-12-04 12:50:47 -0500

    Part 4.

--- src/com/numberengineer/aoc/Main.java
+++ src/com/numberengineer/aoc/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(Math.pow(2,63));
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Math.pow(2,31));
+        System.out.println(Integer.MAX_VALUE);
+//        for (int i = 0; i < 4; i++) {
+//            System.out.println(Math.ceil((i % 3-0.5f) /2));
+//        }
     }
 }
