06dde014945bd43eba714d0b9046bada06507089
marath007 <36909715+marath007@users.noreply.github.com>
2020-12-08 11:34:32 -0500

    Part 4.

--- src/com/numberengineer/aoc/Main.java
+++ src/com/numberengineer/aoc/Main.java
@@ -3,13 +3,10 @@ package com.numberengineer.aoc;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println(Math.pow(2,63));
-        System.out.println(Long.MAX_VALUE);
-        System.out.println(Math.pow(2,31));
-        System.out.println(Integer.MAX_VALUE);
-//        for (int i = 0; i < 4; i++) {
-//            System.out.println(Math.ceil((i % 3-0.5f) /2));
-//        }
+        final var b = (int) '↑';
+
+        "↑".getBytes();
+                String s="↑";
+        System.out.println((char)"↑".charAt(0));
     }
 }
