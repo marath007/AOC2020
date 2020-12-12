ec582fa970e882922f73d7575354eb47d512e168
marath007 <36909715+marath007@users.noreply.github.com>
2020-12-12 18:32:29 -0500

    Part 11

--- src/com/numberengineer/aoc/Aoc.java
+++ src/com/numberengineer/aoc/Aoc.java
@@ -2,226 +2,126 @@ package com.numberengineer.aoc;
 
 
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.function.Consumer;
 
 import static com.numberengineer.aoc.AocPostCompetition.readFile;
 
 class Aoc {
     public static void main(String[] args) {
-        day11();
+        day12();
     }
 
-
-    public static void day11() {
+    public static void day12() {
         TikTok tikTok = new TikTok(true);
-        enum State {
-            EMPTY('L'), OCCUPIED('#'), FLOOR('.'), AIR('~');
-            final char c;
+        enum Cardinal {
+            N, E, S, W;
 
-            State(char c) {
-                this.c = c;
+            Cardinal right(int deg) {
+                final var values = values();
+                return values[(this.ordinal() + deg / 90) % values.length];
             }
 
-            static State of(int i) {
-                return switch (i) {
-                    case 'L' -> EMPTY;
-                    case '.' -> FLOOR;
-                    case '#' -> OCCUPIED;
-                    default -> throw new IllegalStateException("Unexpected value: " + i);
-                };
+            Cardinal left(int deg) {
+                final var values = values();
+                return values[(this.ordinal() + values.length - deg / 90) % values.length];
             }
         }
-        class Grid implements Iterable<State> {
-            final State[][] states;
-            final int width;
-            final int heigth;
-            private final boolean transposed;
+        enum Action {
+            N, E, S, W, L, R, F;
+        }
 
-            public Grid(State[][] states, boolean transposed) {
-                this.states = states;
-                if (transposed) {
-                    heigth = states.length;
-                    width = states[1].length;
-                } else {
-                    width = states.length;
-                    heigth = states[1].length;
-                }
-                this.transposed = transposed;
-            }
+        record Instruction(Action a, int qty) {
 
-            State of(int x, int y) {
-                if (x < 0 || y < 0 || y >= heigth || x >= width) {
-                    return State.AIR;
-                }
-                if (transposed) {
-                    return states[y][x];
-                } else {
-                    return states[x][y];
-                }
-            }
-
-            State[] visibleAdjacentOf(int x, int y) {
-                State[] states = new State[8];
-                int index = 0;
-                record Vector(int dx, int dy) {
-
-                }
-
-                for (int i = -1; i <= 1; i++) {
-                    for (int j = -1; j <= 1; j++) {
-                        if (i == 0 && j == 0) {
-                            continue;
-                        }
-                        final var vector = new Vector(i, j);
-                        int increment = 0;
-                        State of;
-                        do {
-                            increment++;
-                            of = of(x + increment * vector.dx, y + increment * vector.dy);
-                        } while (of.equals(State.FLOOR));
-                        states[index++] = of;
+        }
+        class Waypoint {
+            int x = 10;
+            int y = 1;
+            void rotate(int deg) {
+                final var x = this.x;
+                final var y = this.y;
+                deg=(deg+360)%360;
+                switch (deg) {
+                    case 90 -> {
+                        this.y = -x;
+                        this.x = y;
+                    }
+                    case 180 -> {
+                        this.y = -y;
+                        this.x = -x;
+                    }
+                    case 270 -> {
+                        this.y = x;
+                        this.x = -y;
                     }
                 }
-                return states;
             }
+        }
+        class Ship {
+            int x;
+            int y;
+            Cardinal direction = Cardinal.E;
+            Waypoint waypoint = new Waypoint();
 
-            State[] adjacentsOf(int x, int y) {
-                State[] states = new State[8];
-                int index = 0;
-                for (int i = -1; i <= 1; i++) {
-                    for (int j = -1; j <= 1; j++) {
-                        if (i == 0 && j == 0) {
-                            continue;
-                        }
-                        states[index++] = of(x + i, y + j);
-                    }
-                }
-                return states;
-            }
-
-            State becomes(int x, int y, int tolerance, boolean nearbor) {
-                final State[] states;
-                if (nearbor) {
-                    states = adjacentsOf(x, y);
-                } else {
-                    states = visibleAdjacentOf(x, y);
-                }
-                final var of = of(x, y);
-                switch (of) {
-                    case EMPTY -> {
-                        final var count = Arrays.stream(states).filter(state -> state == State.OCCUPIED).count();
-                        if (count == 0) {
-                            return State.OCCUPIED;
-                        } else {
-                            return State.EMPTY;
+            void processInstruction(Instruction instruction) {
+                switch (instruction.a) {
+                    case N -> y += instruction.qty;
+                    case E -> x += instruction.qty;
+                    case S -> y -= instruction.qty;
+                    case W -> x -= instruction.qty;
+                    case L -> direction = direction.left(instruction.qty);
+                    case R -> direction = direction.right(instruction.qty);
+                    case F -> {
+                        switch (direction) {
+                            case N -> y += instruction.qty;
+                            case E -> x += instruction.qty;
+                            case S -> y -= instruction.qty;
+                            case W -> x -= instruction.qty;
                         }
                     }
-                    case OCCUPIED -> {
-                        final var count = Arrays.stream(states).filter(state -> state == State.OCCUPIED).count();
-                        if (count >= tolerance) {
-                            return State.EMPTY;
-                        } else {
-                            return State.OCCUPIED;
-                        }
-                    }
-                    case FLOOR -> {
-                        return State.FLOOR;
-                    }
-                    default -> throw new IllegalStateException("Unexpected value: " + of);
                 }
             }
 
-            @Override
-            public Iterator<State> iterator() {
-                return null;
-            }
-
-            @Override
-            public void forEach(Consumer<? super State> action) {
-                for (int i = 0; i < width; i++) {
-                    for (int j = 0; j < heigth; j++) {
-                        action.accept(of(i, j));
+            void processWaypointInstruction(Instruction instruction) {
+                switch (instruction.a) {
+                    case N -> waypoint.y += instruction.qty;
+                    case E -> waypoint.x += instruction.qty;
+                    case S -> waypoint.y -= instruction.qty;
+                    case W -> waypoint.x -= instruction.qty;
+                    case L -> waypoint.rotate(-instruction.qty);
+                    case R -> waypoint.rotate(instruction.qty);
+                    case F -> {
+                        x+=waypoint.x*instruction.qty;
+                        y+=waypoint.y*instruction.qty;
                     }
                 }
             }
 
-            @Override
-            public String toString() {
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < heigth; i++) {
-                    for (int j = 0; j < width; j++) {
-                        sb.append(of(j, i).c);
-                    }
-                    sb.append('\n');
-                }
-                return sb.toString();
+            int getManateeDistance() {
+                return Math.abs(x) + Math.abs(y);
             }
         }
         var o = new Object() {
-            int part1 = 0;
-            int part2 = 0;
+            long part1 = 0;
+            long part2 = 0;
         };
-        String data = readFile("F:\\DevFolder\\IdeaProjects\\AdventOfCode\\src\\com\\numberengineer\\aoc\\day11.txt");
-//        String data = "L.LL.LL.LL\n" +
-//                "LLLLLLL.LL\n" +
-//                "L.L.L..L..\n" +
-//                "LLLL.LL.LL\n" +
-//                "L.LL.LL.LL\n" +
-//                "L.LLLLL.LL\n" +
-//                "..L.L.....\n" +
-//                "LLLLLLLLLL\n" +
-//                "L.LLLLLL.L\n" +
-//                "L.LLLLL.LL";
-        final var split = Arrays.stream(data.split("\n")).map(String::chars)
-                .map(intStream -> intStream.mapToObj(State::of).toArray(State[]::new)).toArray(State[][]::new);
-        Grid grid = new Grid(split, true);
-
-        boolean changed;
-        do {
-            State[][] states = new State[grid.width][grid.heigth];
-            changed = false;
-            for (int i = 0; i < grid.width; i++) {
-                for (int j = 0; j < grid.heigth; j++) {
-                    final var becomes = grid.becomes(i, j, 4, true);
-                    if (grid.of(i, j) != becomes) {
-                        changed = true;
-                    }
-                    states[i][j] = becomes;
-                }
-            }
-            grid = new Grid(states, false);
-        } while (changed);
-        grid.forEach(state -> {
-            if (state == State.OCCUPIED) {
-                o.part1++;
-            }
-        });
-        System.out.println("p1");
-        grid = new Grid(split, true);
-        do {
-            State[][] states = new State[grid.width][grid.heigth];
-            changed = false;
-            for (int i = 0; i < grid.width; i++) {
-                for (int j = 0; j < grid.heigth; j++) {
-                    final var becomes = grid.becomes(i, j, 5, false);
-                    if (grid.of(i, j) != becomes) {
-                        changed = true;
-                    }
-                    states[i][j] = becomes;
-                }
-            }
-            grid = new Grid(states, false);
-        } while (changed);
-        grid.forEach(state -> {
-            if (state == State.OCCUPIED) {
-                o.part2++;
-            }
-        });
+        String data = readFile("F:\\DevFolder\\IdeaProjects\\AdventOfCode\\src\\com\\numberengineer\\aoc\\day12.txt");
+//        String data = "F10\n" +
+//                "N3\n" +
+//                "F7\n" +
+//                "R90\n" +
+//                "F11";
+        final var split = data.split("\n");
+        final var instructions = Arrays.stream(split).map(s -> new Instruction(Action.valueOf(s.substring(0, 1)), Integer.parseInt(s.substring(1)))).toArray(Instruction[]::new);
+        Ship ship = new Ship();
+        Arrays.stream(instructions).forEach(ship::processInstruction);
+        o.part1 = ship.getManateeDistance();
+        ship = new Ship();
+        Arrays.stream(instructions).forEach(ship::processWaypointInstruction);
+        o.part2 = ship.getManateeDistance();
         final var methodName = Thread.currentThread().getStackTrace()[1].getMethodName();
         System.out.println(methodName + " part1 " + o.part1);
         System.out.println(methodName + " part2 " + o.part2);
         tikTok.toc(System.out, " " + methodName);
     }
+
 
 }
