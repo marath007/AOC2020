b11f65153cca80b272c42f75c3a24578101ed85f
marath007 <36909715+marath007@users.noreply.github.com>
2022-12-12 12:37:30 -0500

    plenty

--- src/com/numberengineer/aoc2022/days/Day12.java
+++ src/com/numberengineer/aoc2022/days/Day12.java
@@ -2,6 +2,8 @@ package com.numberengineer.aoc2022.days;
 
 import com.numberengineer.aoc.TikTok;
 
+import java.awt.*;
+
 import static com.numberengineer.aoc.Utils.*;
 
 public class Day12 {
@@ -10,8 +12,8 @@ public class Day12 {
     public static void day12() {
         TikTok tikTok = new TikTok(true);
         final var day = getDay();
-        boolean testMode = true;
-//        boolean testMode = false;
+//        boolean testMode = true;
+        boolean testMode = false;
         String data = "Sabqponm\n" +
                 "abcryxxl\n" +
                 "accszExk\n" +
@@ -39,9 +41,7 @@ public class Day12 {
                 } else if (c == 'E') {
                     mountainMap[i][j] = 26;
                     trailPath.end = new Point(i, j);
-                } else if (c == 'a') {
-                    mountainMap[i][j] = 1;
-                } else {
+                }else {
                     mountainMap[i][j] = (c - 'a') + 1;
                 }
             }
@@ -86,7 +86,85 @@ public class Day12 {
             dist++;
             System.out.println("iterations: "+dist);
         }
+
         o.part1 = wMap[trailPath.end.x][trailPath.end.y]-1;
+
+        for (int i = 0; i < strings.length; i++) {
+            for (int j = 0; j < strings[i].length(); j++) {
+                final var c = strings[i].charAt(j);
+                if (c == 'S') {
+                    mountainMap[i][j] = 1;
+                    wMap[i][j] = 1;
+                } else if (c == 'E') {
+                    mountainMap[i][j] = 26;
+                    trailPath.end = new Point(i, j);
+                } else if (c == 'a') {
+                    mountainMap[i][j] = 1;
+                    wMap[i][j] = 1;
+                } else {
+                    mountainMap[i][j] = (c - 'a') + 1;
+                }
+            }
+        }
+
+
+        dist = 0;
+        while (wMap[trailPath.end.x][trailPath.end.y] == 0 || dist<wMap[trailPath.end.x][trailPath.end.y]) {
+            for (int i = 0; i < wMap.length; i++) {
+                for (int j = 0; j < wMap[i].length; j++) {
+                    if (wMap[i][j] != 0) {
+                        int h = mountainMap[i][j];
+                        if (i != 0) {
+                            if (wMap[i - 1][j] == 0) {
+                                if (mountainMap[i - 1][j] <= h + 1) {
+                                    wMap[i - 1][j] = wMap[i][j] + 1;
+                                }
+                            } else {
+                                if (mountainMap[i - 1][j] <= h + 1) {
+                                    wMap[i - 1][j] = Math.min(wMap[i][j] + 1, wMap[i - 1][j]);
+                                }
+                            }
+                        }
+                        if (i != wMap.length - 1) {
+                            if (wMap[i + 1][j] == 0) {
+                                if (mountainMap[i + 1][j] <= h + 1) {
+                                    wMap[i + 1][j] = wMap[i][j] + 1;
+                                }
+                            } else {
+                                if (mountainMap[i + 1][j] <= h + 1) {
+                                    wMap[i + 1][j] = Math.min(wMap[i][j] + 1, wMap[i + 1][j]);
+                                }
+                            }
+                        }
+                        if (j != 0) {
+                            if (wMap[i][j - 1] == 0) {
+                                if (mountainMap[i][j - 1] <= h + 1) {
+                                    wMap[i][j - 1] = wMap[i][j] + 1;
+                                }
+                            } else {
+                                if (mountainMap[i][j - 1] <= h + 1) {
+                                    wMap[i][j - 1] = Math.min(wMap[i][j] + 1, wMap[i][j - 1]);
+                                }
+                            }
+                        }
+                        if (j != wMap[i].length - 1) {
+                            if (wMap[i][j + 1] == 0) {
+                                if (mountainMap[i][j + 1] <= h + 1) {
+                                    wMap[i][j + 1] = wMap[i][j] + 1;
+                                }
+                            } else {
+                                if (mountainMap[i][j + 1] <= h + 1) {
+                                    wMap[i][j + 1] = Math.min(wMap[i][j] + 1,wMap[i][j + 1]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            dist++;
+            System.out.println("iterations: " + dist);
+        }
+        o.part2 = wMap[trailPath.end.x][trailPath.end.y] - 1;
         endOfWork(tikTok, day, testMode, o.part1, o.part2);
     }
 
